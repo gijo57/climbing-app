e4a9fe0482eb7da959566c9d70b3f931cfb93409
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-10-13 16:22:16 +0200

    initial sign up form styles

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,33 +1,99 @@
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import styled from 'styled-components';
+
+const Form = styled(Box)`
+  margin: 3em auto;
+  padding: 3em;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 50%;
+  border: 1px solid grey;
+  border-radius: 20px;
+`;
+
+const Names = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const Field = styled(TextField)`
+  && {
+    margin: 1em 2em;
+  }
+`;
 
 const SignUp = () => {
   return (
-    <form>
-      <TextField id="firstName" label="First Name" variant="filled" required />
-      <TextField id="lastName" label="Last Name" variant="filled" required />
-      <TextField id="username" label="Username" variant="filled" required />
-      <TextField
+    <Form component="form">
+      <Names>
+        <Field
+          id="firstName"
+          label="First Name"
+          variant="filled"
+          margin="normal"
+          size="small"
+          fullWidth
+          required
+        />
+        <Field
+          id="lastName"
+          label="Last Name"
+          variant="filled"
+          margin="normal"
+          size="small"
+          fullWidth
+          required
+        />
+      </Names>
+      <Field
+        id="username"
+        label="Username"
+        variant="filled"
+        margin="normal"
+        size="small"
+        fullWidth
+        required
+        error={false}
+      />
+      <Field
         id="email"
         label="Email"
         variant="filled"
+        margin="normal"
         type="email"
+        size="small"
+        fullWidth
+        error={false}
         required
       />
-      <TextField
+      <Field
         id="password"
         label="Password"
         variant="filled"
+        margin="normal"
         type="password"
+        size="small"
+        fullWidth
         required
+        error={false}
       />
-      <TextField
+      <Field
         id="repeatPassword"
         label="Repeat password"
         variant="filled"
+        margin="normal"
         type="password"
+        size="small"
+        fullWidth
+        error={false}
         required
       />
-    </form>
+      <Button>Sign Up</Button>
+    </Form>
   );
 };
 
