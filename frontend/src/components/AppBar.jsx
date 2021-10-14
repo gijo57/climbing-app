36684024c5d4582a510669c41b457526f824dce8
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-10-14 22:55:14 +0200

    refactor bottom nav to bottonavigation mui element

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -1,41 +1,39 @@
-import AppBarElem from '@mui/material/AppBar';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Paper from '@mui/material/Paper';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import HomeIcon from '@mui/icons-material/Home';
 import styled from 'styled-components';
 
-const IconGroup = styled.div`
-  display: flex;
-  justify-content: space-around;
-  padding: 0.5em 1em;
+const BottomNav = styled(BottomNavigation)`
   @media screen and (min-width: 600px) {
     display: none;
   }
 `;
 
-const IconWrapper = styled.div`
-  margin: 0;
-  padding: 0;
-  font-size: 2.5em;
+const BottomNavAction = styled(BottomNavigationAction)`
+  font-size: 2em;
 `;
 
 const AppBar = () => {
   return (
-    <AppBarElem sx={{ top: 'auto', bottom: 0 }} position="fixed">
-      <IconGroup>
-        <IconWrapper>
-          <HomeIcon fontSize="inherit" />
-        </IconWrapper>
-        <IconWrapper>
-          <HomeIcon fontSize="inherit" />
-        </IconWrapper>
-        <IconWrapper>
-          <AccountCircleIcon fontSize="inherit" />
-        </IconWrapper>
-        <IconWrapper>
-          <AccountCircleIcon fontSize="inherit" />
-        </IconWrapper>
-      </IconGroup>
-    </AppBarElem>
+    <Paper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNav showLabels sx={{ top: 'auto', bottom: 0 }}>
+        <BottomNavAction label="Home" icon={<HomeIcon fontSize="inherit" />} />
+        <BottomNavAction label="As" icon={<HomeIcon fontSize="inherit" />} />
+        <BottomNavAction
+          label="asd"
+          icon={<AccountCircleIcon fontSize="inherit" />}
+        />
+        <BottomNavAction
+          label="as"
+          icon={<AccountCircleIcon fontSize="inherit" />}
+        />
+      </BottomNav>
+    </Paper>
   );
 };
 
