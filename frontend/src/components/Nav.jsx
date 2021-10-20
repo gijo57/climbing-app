16dbcaa1ebf5b98da5beb981bc8ba8e4b9650ede
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-10-20 15:53:20 +0200

    add backdrop when sidebar is open

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SideNavBar from './SideNavBar';
 import BottomNavBar from './BottomNavBar';
 import TopNavBar from './TopNavBar';
+import Backdrop from '@mui/material/Backdrop';
 
 const Nav = ({ onLogout }) => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
@@ -12,15 +13,22 @@ const Nav = ({ onLogout }) => {
 
   return (
     <div>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={isSideBarOpen}
+        onClick={handleSideBarOpen}
+      >
+        <SideNavBar
+          isOpen={isSideBarOpen}
+          onSideBarOpen={handleSideBarOpen}
+          onLogout={onLogout}
+        />
+      </Backdrop>
       <TopNavBar
         isSideBarOpen={isSideBarOpen}
         onSideBarOpen={handleSideBarOpen}
       />
-      <SideNavBar
-        isOpen={isSideBarOpen}
-        onSideBarOpen={handleSideBarOpen}
-        onLogout={onLogout}
-      />
+
       <h1>Hello World!</h1>
       <BottomNavBar />
     </div>
