149dc33ef2748c5c70f8100164b6131aa8c016f5
Pekka Tiitinen <pekka-1992@hotmail.com>
2021-10-22 16:55:49 +0200

    add get user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const User = require('../models/user');
 
 router.get('/', async (req, res, next) => {
-  //
+  const id = req.session.userId;
+  const user = await User.findById(id);
+  res.json(user);
 });
 
 module.exports = router;
