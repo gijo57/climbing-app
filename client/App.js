591b16a7510f44adedf037c6801f1015eb5c3cfd
Pekka Tiitinen <pekka-1992@hotmail.com>
2022-01-18 20:37:35 +0100

    fix submodule issue

---
+++ client/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import theme from './src/theme';
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
