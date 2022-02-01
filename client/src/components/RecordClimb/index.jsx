aec649d03a3d074ec21ce23949082bafec8d9bbc
Pekka Tiitinen <pekka-1992@hotmail.com>
2022-02-01 20:50:29 +0100

    add tag addition feature

--- client/src/components/RecordClimb/index.jsx
+++ client/src/components/RecordClimb/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import {
   Modal,
   Portal,
@@ -11,6 +11,7 @@ import {
   Chip,
   useTheme
 } from 'react-native-paper';
+import { Icon } from 'react-native-elements';
 import Grade from './Grade';
 
 const styles = StyleSheet.create({
@@ -39,6 +40,35 @@ const styles = StyleSheet.create({
   },
   tag: {
     margin: 2
+  },
+  addTagButton: {
+    width: 10,
+    borderRadius: 50,
+    margin: 2,
+    backgroundColor: '#EBEBEB'
+  },
+  newTagField: {
+    width: 100,
+    position: 'absolute',
+    padding: 10,
+    paddingRight: 35,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#EBEBEB',
+    borderRadius: 50,
+    margin: 2,
+    color: 'grey'
+  },
+  newTagFieldWrapper: {
+    width: 100,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  newTagFieldAddIcon: {
+    marginHorizontal: 10
   }
 });
 
@@ -51,13 +81,9 @@ const RecordClimb = () => {
   const [gradingSystem, setGradingSystem] = useState(null);
   const [isAscended, setIsAscended] = useState(false);
   const [ascendType, setAscendType] = useState(null);
-  const [tags, setTags] = useState([
-    'hurr',
-    'durrrrurrur',
-    'asdasd',
-    'asdasdasd',
-    'asdasdasd'
-  ]);
+  const [showAddNewTagField, setShowAddNewTagField] = useState(false);
+  const [newTag, setNewTag] = useState('');
+  const [tags, setTags] = useState([]);
   const [notes, setNotes] = useState('');
 
   const handleSystemChange = (system) => {
@@ -68,7 +94,12 @@ const RecordClimb = () => {
     setGrade(grade);
   };
 
-  const handleTagAddition = () => {};
+  const handleTagAddition = () => {
+    const updatedTags = [...tags, newTag];
+    setTags(updatedTags);
+    setNewTag('');
+    setShowAddNewTagField(false);
+  };
 
   const handleTagRemoval = (tagToRemove) => {
     const updatedTags = tags.filter((tag) => tag !== tagToRemove);
@@ -83,78 +114,102 @@ const RecordClimb = () => {
           onDismiss={hideModal}
           contentContainerStyle={styles.modal}
         >
-          <View style={switchWrapperStyle}>
-            <Text>Ascended</Text>
-            <Switch
-              value={isAscended}
-              onValueChange={() => {
-                setIsAscended(!isAscended);
-              }}
+          <ScrollView>
+            <View style={switchWrapperStyle}>
+              <Text>Ascended</Text>
+              <Switch
+                value={isAscended}
+                onValueChange={() => {
+                  setIsAscended(!isAscended);
+                }}
+              />
+            </View>
+            {isAscended && (
+              <ToggleButton.Row
+                style={styles.ascendTypeWrapper}
+                onValueChange={(value) => setAscendType(value)}
+                value={ascendType}
+              >
+                <ToggleButton
+                  style={styles.ascendTypeButton}
+                  icon={() => (
+                    <View>
+                      <Text>Onsight</Text>
+                    </View>
+                  )}
+                  value="Onsight"
+                />
+                <ToggleButton
+                  style={styles.ascendTypeButton}
+                  icon={() => (
+                    <View>
+                      <Text>Flash</Text>
+                    </View>
+                  )}
+                  value="Flash"
+                />
+                <ToggleButton
+                  style={styles.ascendTypeButton}
+                  icon={() => (
+                    <View>
+                      <Text>Redpoint</Text>
+                    </View>
+                  )}
+                  value="Redpoint"
+                />
+                <ToggleButton
+                  style={styles.ascendTypeButton}
+                  icon={() => (
+                    <View>
+                      <Text>Repeat</Text>
+                    </View>
+                  )}
+                  value="Repeat"
+                />
+              </ToggleButton.Row>
+            )}
+            <Divider />
+            <Grade
+              onSystemChange={handleSystemChange}
+              onGradeChange={handleGradeChange}
             />
-          </View>
-          {isAscended && (
-            <ToggleButton.Row
-              style={styles.ascendTypeWrapper}
-              onValueChange={(value) => setAscendType(value)}
-              value={ascendType}
-            >
-              <ToggleButton
-                style={styles.ascendTypeButton}
-                icon={() => (
-                  <View>
-                    <Text>Onsight</Text>
-                  </View>
-                )}
-                value="Onsight"
-              />
-              <ToggleButton
-                style={styles.ascendTypeButton}
-                icon={() => (
-                  <View>
-                    <Text>Flash</Text>
-                  </View>
-                )}
-                value="Flash"
-              />
-              <ToggleButton
-                style={styles.ascendTypeButton}
-                icon={() => (
-                  <View>
-                    <Text>Redpoint</Text>
-                  </View>
-                )}
-                value="Redpoint"
-              />
-              <ToggleButton
-                style={styles.ascendTypeButton}
-                icon={() => (
-                  <View>
-                    <Text>Repeat</Text>
-                  </View>
-                )}
-                value="Repeat"
-              />
-            </ToggleButton.Row>
-          )}
-          <Divider />
-          <Grade
-            onSystemChange={handleSystemChange}
-            onGradeChange={handleGradeChange}
-          />
-          <Divider />
-          <View style={styles.tagsWrapper}>
-            {tags.map((tag) => (
-              <Chip onClose={() => handleTagRemoval(tag)} style={styles.tag}>
-                {tag}
-              </Chip>
-            ))}
-            <Chip onPress={handleTagAddition} style={styles.tag}>
-              +
-            </Chip>
-          </View>
-          <Button mode="contained" style={buttonStyle}>
-            Save climb
-          </Button>
+            <Divider />
+            <View style={styles.tagsWrapper}>
+              {tags.map((tag) => (
+                <Chip
+                  key={tag}
+                  onClose={() => handleTagRemoval(tag)}
+                  style={styles.tag}
+                >
+                  {tag}
+                </Chip>
+              ))}
+              {(showAddNewTagField && (
+                <View style={styles.newTagFieldWrapper}>
+                  <TextInput
+                    style={styles.newTagField}
+                    value={newTag}
+                    onChangeText={(value) => setNewTag(value)}
+                  />
+                  <Icon
+                    onPress={handleTagAddition}
+                    name="check"
+                    iconStyle={styles.newTagFieldAddIcon}
+                  />
+                </View>
+              )) || (
+                <Button
+                  onPress={() => setShowAddNewTagField(true)}
+                  mode="text"
+                  icon="plus"
+                  style={styles.addTagButton}
+                />
+              )}
+            </View>
+            <Button mode="contained" style={buttonStyle}>
+              Save climb
+            </Button>
+          </ScrollView>
         </Modal>
       </Portal>
       <Button mode="contained" style={buttonStyle} onPress={showModal}>
